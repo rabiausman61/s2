b209a3d6e958bddfafb0bb7ffb2e6302862d40f4
rabiausman61 <81038373+rabiausman61@users.noreply.github.com>
2021-05-18 17:29:13 +0500

    s2

--- s.js
+++ s.js
@@ -1,10 +1,10 @@
 var selectedRow = null
-
 function onFormSubmit() {
     if (validate()) {
         var formData = readFormData();
-        
-       
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
             updateRecord(formData);
         resetForm();
     }
