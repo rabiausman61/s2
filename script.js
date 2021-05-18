29305e6947745803880f2ae462f6a9ff8d0ab0ab
rabiausman61 <81038373+rabiausman61@users.noreply.github.com>
2021-05-18 17:31:00 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ var selectedRow = null
 function onFormSubmit() {
     if (validate()) {
         var formData = readFormData();
-        
-       
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
             updateRecord(formData);
         resetForm();
     }
